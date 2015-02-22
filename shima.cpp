e0827867ea6ce31558600d909345aaad96ac5690
Shima <szzzdhrd@memphis.edu>
2015-02-22 14:40:45 -0600

    C++ code that does nothing

--- shima.cpp
+++ shima.cpp
@@ -1,1 +1,26 @@
-Hi
+/*
+ * H4 Program
+ * To Demonstrate That You Have Secured Access to the Computer Resources necessary to be Successful in the Course,
+ * And How to Design and Use Functions
+ * By Shima Azizzadeh-Roodpish
+ * 22 Feb 2015
+ * No Copyright
+ * Github account: https://github.com/Shima63/H4.git
+ */
+ 
+// External Libraries
+ 
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <iomanip>
+using namespace std;
+
+// Main Program.
+// Return Zero on Success, Non-Zero in case of Failure.
+
+int main () {
+
+    return 0;
+}
+ 
